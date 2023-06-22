b95ab720c0a6c0fda832164345d9a635cf344de6
talhazaidi131313 <71030605+talhazaidi131313@users.noreply.github.com>
2023-06-22 18:37:29 -0500

    Delete gym_env_check.py

--- gym_env_check.py
+++
@@ -1,7 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from Spacecraft_Env import Spacecraft_Env
-
-
-env = Spacecraft_Env()
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
